a949d0d7b03d54c85e9042469390c6d3678e8813
hasanRafi2002 <rafi2002ten@gmail.com>
2024-12-09 06:08:35 +0600

    a

--- pages/api/component.js
+++ pages/api/component.js
@@ -1,18 +1,21 @@
-import { connectToDatabase } from '../../lib/mongo';
+// pages/api/component.js
+import { MongoClient } from 'mongodb';
 
 export default async function handler(req, res) {
-  const { db } = await connectToDatabase();
-  const collection = db.collection('component');
+  const client = new MongoClient(process.env.MONGO_URI);
+  await client.connect();
+  const db = client.db('myDatabase');
+  const componentCollection = db.collection('component');
 
-  try {
-    if (req.method === 'GET') {
-      const componentData = await collection.find().toArray();
+  if (req.method === 'GET') {
+    try {
+      const componentData = await componentCollection.find().toArray();
       res.status(200).json(componentData);
-    } else {
-      res.status(405).json({ message: 'Method Not Allowed' });
+    } catch (error) {
+      console.error('Error retrieving component data:', error);
+      res.status(500).json({ message: 'Error retrieving component data' });
     }
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ message: 'Error retrieving component data', error: error.message });
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
   }
 }
